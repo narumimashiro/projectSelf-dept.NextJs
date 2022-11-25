5d88e997d8155b0a5750b364cf6bce2c11ca750f
Narumi_Mashiro <narunarumashiro96@gmail.com>
2022-11-26 03:45:45 +0900

    カリキュラム進行上Navberのレイアウト変更が必要だった

--- src/components/layouts/navbar.tsx
+++ src/components/layouts/navbar.tsx
@@ -10,13 +10,16 @@ const Navbar = ({ children }: Props) => {
   return (
     <>
       <nav className={style.container}>
-        <ul>
-          <li><Link href="/">Top</Link></li>
-          <li><Link href="/main/profile">Profile</Link></li>
-          <li><Link href="https://pjsekai.sega.jp/" target="_blank">prsk</Link></li>
-          <li><Link href="https://genshin.hoyoverse.com/ja" target="_blank">Genshin</Link></li>
-          <li><Link href="https://playvalorant.com/ja-jp/?utm_source=riotbar&utm_medium=productcard%2Bplayvalorant.com&utm_campaign=val&utm_content=ep5act1" target="_blank">Valorant</Link></li>
-        </ul>
+          <div className={style["to-top"]}>
+            <Link href="/">Top</Link>
+          </div>
+          <div className={style["nav-cont"]}>
+            <li><Link href="/main/profile">Profile</Link></li>
+            <li><Link href="/main/news">News</Link></li>
+            <li><Link href="https://pjsekai.sega.jp/" target="_blank">prsk</Link></li>
+            <li><Link href="https://genshin.hoyoverse.com/ja" target="_blank">Genshin</Link></li>
+            <li><Link href="https://playvalorant.com/ja-jp/?utm_source=riotbar&utm_medium=productcard%2Bplayvalorant.com&utm_campaign=val&utm_content=ep5act1" target="_blank">Valorant</Link></li>
+          </div>
       </nav>
       { children }
     </>
