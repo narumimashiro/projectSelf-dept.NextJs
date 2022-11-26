860ee8ac645e2fea13db0c7776e62a6dd35f9e10
Narumi_Mashiro <narunarumashiro96@gmail.com>
2022-11-26 15:45:14 +0900

    madd mikufooter component

--- src/components/layouts/navbar.tsx
+++ src/components/layouts/navbar.tsx
@@ -1,6 +1,9 @@
 import Link from "next/link";
 import { ReactNode } from "react";
-import style from 'styles/Navbar.module.sass'
+import styles from './Navbar.module.sass'
+
+// todo delete
+import FooterMiku from './FooterMiku'
 
 interface Props {
   children: ReactNode;
@@ -9,11 +12,11 @@ interface Props {
 const Navbar = ({ children }: Props) => {
   return (
     <>
-      <nav className={style.container}>
-        <div className={style["to-top"]}>
+      <nav className={styles.container}>
+        <div className={styles["to-top"]}>
           <Link href="/">Top</Link>
         </div>
-        <div className={style["nav-cont"]}>
+        <div className={styles["nav-cont"]}>
           <li><Link href="/main/profile">Profile</Link></li>
           <li><Link href="/main/news">News</Link></li>
           <li><Link href="https://pjsekai.sega.jp/" target="_blank">prsk</Link></li>
