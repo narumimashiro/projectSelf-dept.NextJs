a27e59dc4e17400c2e7e8b290667e49b50b2b589
Narumi_Mashiro <narunarumashiro96@gmail.com>
2022-12-11 22:24:11 +0900

    animation transition完了

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -1,7 +1,7 @@
-import { useSelector, useDispatch} from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import { RootState } from '@/redux/store'
 import { modalReducer } from '@/redux/modal/modal.reducer'
-import { Transition } from 'react-transition-group'
+import { CSSTransition, Transition } from 'react-transition-group'
 import styles from './Modal.module.sass'
 
 const Modal = () => {
@@ -12,26 +12,31 @@ const Modal = () => {
     dispatch(modalReducer.closeModal())
   }
 
-  if (isModalOpen) {
-    return (
-      <div className={styles.TransitionStyle}>
-        {/* <Transition
-          className={styles.modal}
-          timeout={700}
-          unmountOnExit> */}
-          <div className={styles['modal-overlay']} onClick={closeModal}>
-            <div className={styles['modal-window']}
-                 onClick={(event) => event.stopPropagation()}>
-              <header>This is modal window</header>
-              <div>use this area message board</div>
-              <footer>ok or close button</footer>
-            </div>
+  return (
+    <CSSTransition
+      in={isModalOpen}
+      timeout={1000}
+      unmountOnExit
+      classNames={{
+        enter:       styles['modal-enter'],
+        enterActive: styles['modal-enter-active'],
+        enterDone:   styles['modal-enter-done'],
+        exit      :  styles['modal-exit'],
+        exitActive:  styles['modal-exit-active']
+      }}>
+      <div>
+        <div className={styles['modal-overlay']} onClick={closeModal}>
+          <div className={styles['modal-window']}
+               onClick={(event) => event.stopPropagation()}>
+              <div className={styles['modal-content']}>
+                <header>This is modal window</header>
+                <div>use this area message board</div>
+                <footer>ok or close button</footer>
+              </div>
           </div>
-        {/* </Transition> */}
+        </div>
       </div>
-    )
-  } else {
-    return null
-  }
+    </CSSTransition>
+  )
 }
 export default Modal
