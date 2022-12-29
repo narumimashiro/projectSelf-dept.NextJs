5dfd005ea41b6940ab9eaca7d78e27052babdff7
Narumi_Mashiro <narunarumashiro96@gmail.com>
2022-12-29 23:06:14 +0900

    code block bug fix

--- src/lib/codeblock.tsx
+++ src/lib/codeblock.tsx
@@ -1,12 +1,12 @@
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
-import { oneLight } from 'react-syntax-highlighter/dist/esm/styles/prism'
+import { coldarkCold } from 'react-syntax-highlighter/dist/esm/styles/prism'
 import { CodeComponent } from 'react-markdown/lib/ast-to-react'
 
 const CodeBlock: CodeComponent = ({ inline, className, children }) => {
   const match = /language-(\w+)/.exec(className || '');
   return !inline && match ? (
     <SyntaxHighlighter
-      style={oneLight}
+      style={coldarkCold}
       language={match[1]}
       children={String(children).replace(/\n$/, '')}
     />
