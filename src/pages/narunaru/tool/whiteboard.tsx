bc72ef7ecd00e180c3c07806d4f11c4f589fb07b
Narumi_Mashiro <narunarumashiro96@gmail.com>
2023-02-03 03:16:38 +0900

    TempWhiteBoard作成

--- src/pages/narunaru/tool/whiteboard.tsx
+++ src/pages/narunaru/tool/whiteboard.tsx
@@ -1,12 +1,28 @@
 import Head from 'next/head'
+import ReactSignatureCanvas from 'react-signature-canvas'
 
 const WhiteBoard = () => {
+  const getCanvasStyle = () => ({
+    width: 1600,
+    height: 800,
+    border: '1px solid'
+  })
+
   return (
     <div>
       <Head>
       <title>Tool | WhiteBoard</title>
       <meta name='discription' content='whiteboard you can hand writing'></meta>
       </Head>
+      <div className='flex flex-col justify-center items-center mt-28'>
+        <header>Temp White Board</header>
+        <div style={getCanvasStyle()}>
+          <ReactSignatureCanvas
+            penColor='black'
+            canvasProps={getCanvasStyle()}
+          />
+        </div>
+      </div>
     </div>
   )
 }
